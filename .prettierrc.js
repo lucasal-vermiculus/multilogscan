525c74a044ddccbf9963389b2f6425b47493d403
Lucas Alenbro Lindström <lucas.lindstrom@vermiculus.se>
2025-10-24 11:23:44 +0200

    Updates

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// Renamed to .cjs to resolve compatibility issue with "type": "module" in package.json
+module.exports = {
+    semi: false,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    endOfLine: 'auto',
+}
